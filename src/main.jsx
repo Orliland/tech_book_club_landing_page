7248107e015bc0ea46c1afbd651c0f671025898d
Orlando Verdin <git@orli.land>
2025-01-12 23:52:38 -0700

    install fonts (inter-martian)

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+// Supports weights 100-800
+import "@fontsource-variable/martian-mono";
+
+// Supports weights 100-900
+import "@fontsource-variable/inter";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
