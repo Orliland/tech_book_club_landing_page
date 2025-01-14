b90a23eac9a03357d9debd68497c13f47f76f762
Orlando Verdin <git@orli.land>
2025-01-13 22:55:03 -0700

    add primary button default

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import ArrowDownIcon from "../assets/icon-arrow-down.svg";
+
+export const Button = () => {
+  return (
+    <button className="flex gap-200 items-center justify-center bg-light-salmon-50 border-2 border-neutral-900 rounded-lg py-250 w-full">
+      <span className="text-preset-6">BUTTON</span>
+      {/* TODO: replace alt for button text */}
+      <img src={ArrowDownIcon} alt="Click here" />
+    </button>
+  );
+};
