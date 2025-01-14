e5992ce3be7e25d8adffebf116625f7900666c36
Orlando Verdin <git@orli.land>
2025-01-13 23:16:05 -0700

    add styles for alternate button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,22 @@
 import ArrowDownIcon from "../assets/icon-arrow-down.svg";
+import ArrowUpIcon from "../assets/icon-arrow-up.svg";
 
-export const Button = () => {
+export const Button = ({ children, type = "alternate" }) => {
+  const styles = {
+    // TODO: check if w-full apply too for secondary
+    primary:
+      "bg-light-salmon-50 border-neutral-900 hover:bg-gradient focus:shadow-primary ",
+    alternate: "bg-neutral-900 border-neutral-0", // TODO: remove bg on production
+  };
   return (
-    <button className="flex gap-200 items-center justify-center bg-light-salmon-50 border-2 border-neutral-900 rounded-lg py-250 w-full hover:bg-gradient focus:shadow-primary focus:outline-none">
-      <span className="text-preset-6">BUTTON</span>
-      {/* TODO: replace alt for button text */}
-      <img src={ArrowDownIcon} alt="Click here" />
+    <button
+      className={`flex gap-200 items-center justify-center border-2 rounded-lg py-250 w-full focus:outline-none ${styles[type]}`}
+    >
+      <span className="text-preset-6">{children}</span>
+      <img
+        src={type === "primary" ? ArrowDownIcon : ArrowUpIcon}
+        alt={children}
+      />
     </button>
   );
 };
